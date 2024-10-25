680d03c9d8c0777392250f05e4e77511cbd4d764
Mr.mh021 <moeen.h051@gmail.com>
2024-10-25 16:58:03 +0330

    app

---
+++ app/Models/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use HasFactory;
+
+    protected $fillable =
+        [
+            'name',
+            'family',
+            'address',
+            'phone',
+            'email',
+            'password',
+        ];
+    protected $hidden =
+        [
+          'password'
+        ];
+}
